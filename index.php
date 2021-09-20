3f995832504a052b3ce2558993243cd1c2462fc8
Uda <uda53@mail.ru>
2021-09-20 15:02:24 +0600

    1 commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Заголовок страницы</title>
+</head>
+<body>
+Контент веб-страницы...
+
+    
+<script>
+
+
+</script>
+
+	<!-- Подключаем внешний файл Javascript -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="myscript.js"></script>
+<script src="script.js"></script>
+</body>
+</html>
---
+++ myscript.js
@@ -0,0 +1,36 @@
+let persone = {
+    name: "John",
+    age: 23,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','ke.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("Ку!");
+
+//let answer = confirm("aRE YOU HERE?");
+
+//console.log(answer);
+
+//let answer = prompt("18?", "y");
+
+//console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChacked = true,
+    isClose = false;
+
+console.log(isChacked && isClose);
+
